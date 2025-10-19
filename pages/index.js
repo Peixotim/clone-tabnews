6ae723825af15e509a6eeb2e01a3e5d0509d8c85
Pedro Peixoto <126399622+Peixotim@users.noreply.github.com>
2025-10-19 21:12:24 +0000

    feat: Adiciona uma nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 export default function Home(){
     return (
         <div>
-        <span>Te amo </span>
+        <span><strong>Maria</strong> vai mamar ops amar o Pep , e o <strong>Pedro</strong> vai comer ops , vai amar a <strong> Maria </strong></span>
+        <strong>Te amo mais doque o infinito amor</strong>
         </div>
     )
 }
