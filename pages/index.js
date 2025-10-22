3bdc6f17a1b6f6c301966415e396d1e4ab0d97c1
Pedro Peixoto <126399622+Peixotim@users.noreply.github.com>
2025-10-22 13:44:21 +0000

    Feat: Altera a mensagem da pagina index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-export default function Home(){
-    return (
-        <div>
-        <span><strong>Maria</strong> vai mamar ops amar o Pep , e o <strong>Pedro</strong> vai comer ops , vai amar a <strong> Maria </strong></span>
-        <strong>Te amo mais doque o infinito amor</strong>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <span>Te amo</span>
+    </div>
+  );
 }
