2a0d5ea74be4d2db0e763b32f7713c8f024402f7
Peixotim <pedropeixotovz@gmail.com>
2026-01-28 15:16:59 -0300

    feat: Altera o path de database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,10 @@
-import database from '../../../../infra/database.js'
+import database from "infra/database.js";
 
-async function status(req,res){
-  const result = await database.query('SELECT 1 + 1 as sum');
-  console.log(result.rows)
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   res.status(200).json({
-    message:`Hello`
+    message: `Hello`,
   });
 }
 
