857db849e695a338897256394709e2572221859c
Peixotim <pedropeixotovz@gmail.com>
2026-02-02 13:59:11 -0300

    feat: Finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  res.status(200).json({
-    message: `Hello`,
-  });
+  try {
+    const updatedAt = new Date().toISOString();
+    const versionPostgres = await database.query("SHOW server_version;");
+    const version = versionPostgres.rows[0].server_version;
+
+    const queryMaxConnections = await database.query("SHOW max_connections;");
+    const { max_connections } = queryMaxConnections.rows[0];
+
+    const databaseName = process.env.POSTGRES_DB;
+    const queryConnectionUsed = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+      values: [databaseName],
+    });
+
+    const { count } = queryConnectionUsed.rows[0];
+
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: version,
+          max_connections: parseInt(max_connections),
+          opened_connections: count,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default status;
